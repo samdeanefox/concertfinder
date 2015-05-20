06faebbf464fa51db5dc8dcdf1deebe3a69e205b
samdeanefox <samdeanefox@gmail.com>
2015-05-19 22:10:59 -0700

    request works

--- ConcertFinder.js
+++ ConcertFinder.js
@@ -4,6 +4,14 @@ var http = require('http');
 var apiKey = '832t889gh3n8728fzxthr644';
 var urlStr = 'http://api.jambase.com/events?zipCode=95128&radius=50&page=0&api_key=' + apiKey;
 
+var u = url.parse(urlStr);
+
+var options = {
+	host: u.hostname,
+	path: u.path,
+	port: u.port || 80,
+	method: 'GET'
+};
 
 function responseHandler(res) {
 	var str = '';
@@ -15,3 +23,7 @@ function responseHandler(res) {
 	});
 }
 
+console.log('connecting...');
+
+var req = http.request(options, responseHandler);
+req.end();
